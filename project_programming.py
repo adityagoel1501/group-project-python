65dcfa6691f75d35de73b9c53128638224a73bfa
adityagoel1501 <153238252+adityagoel1501@users.noreply.github.com>
2023-12-07 17:28:24 -0500

    Update project_programming.py

--- project_programming.py
+++ project_programming.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import numpy as np 
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn import metrics
+
 
 # Create a list of pages
 pages = ['Introduction to dataset','Cleaning of dataset','Descriptive Analysis','Prediction of Salary']
